aac94164f4720d542914d69ac32f4156113af2b6
Dylan Parry <me@dylanparry.com>
2016-09-19 17:48:27 +0100

    Add files to enable editing in VS2015 with Node.JS tools and Wallaby.js

---
+++ wallaby.js
@@ -0,0 +1,30 @@
+﻿module.exports = function (wallaby) {
+	var typescriptOptions = {
+		module: 'commonjs',
+		target: 'es6',
+		transpileOnly: true,
+	};
+
+	return {
+		files: [
+			{ pattern: 'src/**/*.ts', load: false },
+			{ pattern: 'src/**/*.spec.ts', ignore: true },
+		],
+
+		tests: [
+			{ pattern: 'src/**/*.spec.ts' },
+			{ pattern: 'src/**/*.spec.tsx' },
+		],
+
+		compilers: {
+			'**/*.ts': wallaby.compilers.typeScript(typescriptOptions),
+		},
+
+		testFramework: 'mocha',
+
+		env: {
+			type: 'node',
+			runner: 'node',
+		},
+	};
+};
