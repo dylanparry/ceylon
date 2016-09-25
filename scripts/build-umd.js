662ba178ff451beb2b39c1d26370d5d1df14701b
Dylan Parry <me@dylanparry.com>
2016-09-25 10:49:49 +0100

    Errors were being swallowed by new build process

--- scripts/build-umd.js
+++ scripts/build-umd.js
@@ -2,13 +2,6 @@ const rollup = require('rollup');
 const nodeResolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs');
 const uglify = require('rollup-plugin-uglify');
-const exec = require('child_process').execSync;
-
-// Transpile the original TypeScript into ES5 with CommonJS modules
-exec('tsc --outDir ./lib --declaration ./src/index.ts');
-
-// Transpile the original TypeScript into ES5 with ES6 modules
-exec('tsc --target es5 --module es2015 --moduleResolution node --outDir ./lib/es6 ./src/index.ts');
 
 // Create a UMD bundle
 rollup.rollup({
