7d458f2f0753f0c55b27a0e4059770163d877f97
Dylan Parry <me@dylanparry.com>
2016-09-26 14:59:17 +0100

    Add comment explaining purpose of file

--- src/umd.ts
+++ src/umd.ts
@@ -1,3 +1,9 @@
+/*
+ * This file exists purely to ensure that there is a single entry point
+ * for the UMD build. It should do nothing more than import 'expect'
+ * and re-export it as the default export with no other named exports.
+ */
+
 import expect from './index';
 
 export default expect;
