0dcf78e23941cfccbb3df89bcff430e725ffd57c
Dylan Parry <me@dylanparry.com>
2016-09-27 08:53:11 +0100

    Simplify exposure of assert

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import IExpect from './interfaces/expect';
 import Expectation from './expectation';
-import assert from './assert';
+export { default as assert } from './assert';
 
 const expect: IExpect = <T>(actual: T): Expectation<T> => {
     return new Expectation(actual);
 };
-
-/*
- * Export non-default objects here so they are exposed on the API,
- * but kept hidden from the UMD build
- */
-export { assert };
 
 export default expect;
